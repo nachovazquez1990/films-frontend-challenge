982a606e5ccef2a89d3808f2f328bbfc3e235d64
nachovazquez1990 <nachovazquez@msn.com>
2025-09-18 02:18:24 +0200

    fix: Type a class on card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,13 @@
 import { Link } from 'react-router-dom'
 import '../styles/components/card.scss';
+import type { FilmCard } from '../lib/types'
 
-type FilmCard = { id: number; title: string; img: string }
-
-export default function Card({ film, to }: { film: FilmCard; to?: string }) {
+export default function Card({ film, to, className }: { film: FilmCard; to?: string; className?: string }) {
     const href = to ?? `/film/${film.id}`
     return (
-        <Link to={href} state={{ film }} className="card">
-            <img src={film.img} alt={film.title} />
+        <Link to={href} state={{ film }} className={`card ${className ?? ''}`}>
+            {film.img && <img src={film.img} alt={film.title} />}
             <div className="title">{film.title}</div>
         </Link>
     )
 }
-
