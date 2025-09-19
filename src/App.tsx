e88ab9b0d7335c661f66772419d73ee6daaa4149
nachovazquez1990 <nachovazquez@msn.com>
2025-09-19 22:41:01 +0200

    add: 404 Not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import Header from './components/Header'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Film from './pages/Film'
 import Wishlist from './pages/Wishlist'
+import NotFound from './pages/404'
 
 export default function App() {
   return (
-    <div className="container">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/film/:id" element={<Film />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/film/:id" element={<Film />} />
+      <Route path="/wishlist" element={<Wishlist />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   )
 }
---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div>
+            <h2>404 — Not Found</h2>
+        </div>
+    )
+}
